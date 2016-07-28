b1a365d929fbb72731ace4bad73fe6b782718f88
yishai <yishai5555@gmail.com>
2016-07-28 11:06:56 +0300

    b2

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,25 +6,42 @@ import {Hero} from './Hero';
     template: `
     
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+    <li *ngFor="let hero of heroes">
+    <span class="badge">{{hero.id}}</span>-{{hero.name}}
+    </li>
+    
+</ul>
+    
+    <!--<h2>{{hero.name}} details!</h2>
     <div>
         <label>id: </label>{{hero.id}}</div>
         <div>
         <label>name: </label>
-        <!--{{hero.name}}-->
-        <!--<input value="{{hero.name}}"  placeholder="name"/>-->
         <input [(ngModel)]="hero.name"  placeholder="name"/>
     </div>
-
-<!--<h1>{{title}}</h1>-->
-<!--<h2>{{hero.name}} details!</h2>-->
+-->
+ 
 `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
 
-    /*;
-     hero = 'Windstorm';*/
+    const heroes:Hero[] = [
+        {id: 11, name: 'Mr. Nice'},
+        {id: 12, name: 'Narco'},
+        {id: 13, name: 'Bombasto'},
+        {id: 14, name: 'Celeritas'},
+        {id: 15, name: 'Magneta'},
+        {id: 16, name: 'RubberMan'},
+        {id: 17, name: 'Dynama'},
+        {id: 18, name: 'Dr IQ'},
+        {id: 19, name: 'Magma'},
+        {id: 20, name: 'Tornado'}
+    ];
+
+    // public heroes=HEROS;
 
     hero:Hero = {
         id: 1,
