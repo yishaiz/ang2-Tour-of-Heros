6b9b0b8dde6ccd2e58ffe7010eeddadd1e099efd
yishai <yishai5555@gmail.com>
2016-07-31 11:14:56 +0300

    b9-temp-good

--- app/hero2.service.js
+++ app/hero2.service.js
@@ -11,21 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 // import  {HEROES} from './mock-heroes';
 //this comment cause nothing is being displayed !!!
-/*
- export const heroes: Hero[] =
- [
- {id: 11, name: 'Mr. Nice'},
- {id: 12, name: 'Narco'},
- {id: 13, name: 'Bombasto'},
- {id: 14, name: 'Celeritas'},
- {id: 15, name: 'Magneta'},
- {id: 16, name: 'RubberMan'},
- {id: 17, name: 'Dynama'},
- {id: 18, name: 'Dr IQ'},
- {id: 19, name: 'Magma'},
- {id: 20, name: 'Tornado'}
- ];
- */
 var Hero2Service = (function () {
     function Hero2Service() {
         this.HEROES1 = [
