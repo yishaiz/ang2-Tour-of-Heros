7e5e603aae4a2957675df3e73d754da29e313787
yishai <yishai5555@gmail.com>
2016-08-01 15:25:33 +0300

    b21

--- app/Heroes/hero-detail.component.ts
+++ app/Heroes/hero-detail.component.ts
@@ -1,8 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
 import {Hero} from './Hero';
+
+import {ActivatedRoute, Router} from "@angular/router";
+import {HeroService} from "./hero.service";
 
 @Component({
     selector: 'my-hero-detail',
+    providers:[HeroService],
     styles: [` 
 `],
     template: `
@@ -16,7 +20,47 @@ import {Hero} from './Hero';
   </div>
 `
 })
-export class HeroDetailComponent {
-    @Input()
+export class HeroDetailComponent implements OnInit, OnDestroy   {
+ /*   @Input()*/
     hero: Hero;
+
+
+
+
+    private subscriber: any;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router:  Router,
+        private service:  HeroService){
+
+    }
+
+
+
+    ngOnInit(){
+        this.subscriber = this.route.params.subscribe(
+            params => {
+                let id = +params['id'];  // (+) converts string 'id' to a number
+                console.log('id', id);
+                this.hero=null;
+                this.service.getHero(id).then(
+                    // debugger;
+                    hero => {
+                        this.hero=hero;
+                        console.log("her = ", this.hero);
+                        console.dir(hero);
+                    }
+
+                )
+            }
+        );
+
+    }
+
+    ngOnDestroy(){
+        console.log('destroy');
+
+        this.subscriber.unsubscribe();
+    }
 }
